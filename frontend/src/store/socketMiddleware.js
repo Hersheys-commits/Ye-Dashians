3a006b690a2796e9595d1e98b075d13c30d18a7e
Harsh Sharma <harsh1618sharma@gmail.com>
2025-02-11 22:27:05 +0530

    Update socketMiddleware.js

--- frontend/src/store/socketMiddleware.js
+++ frontend/src/store/socketMiddleware.js
@@ -14,15 +14,8 @@ export const initializeSocket = (userId) => (dispatch) => {
         console.log("Socket already connected");
         return;
     }
-
-<<<<<<< HEAD
-    const SOCKET_URL =
-        import.meta.env.VITE_BACKEND_URL || "http://localhost:4001";
-
-=======
-    const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || "https://nexus-xwdr.onrender.com";
     
->>>>>>> 0349444146aa112ddb36ac838891388025d4d9b1
+    const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || "https://nexus-xwdr.onrender.com";
     console.log("Initializing socket connection to:", SOCKET_URL);
 
     socket = io(SOCKET_URL, {
